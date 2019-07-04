8f869a332b4db2f1b1aa00f7a79fe94f0ccf8240
akaKAIN <mkain@mail.ru>
2019-07-04 19:25:13 +1000

    Тестовая вьюха для проверки вывода в шаблоны

--- overview/views.py
+++ overview/views.py
@@ -4,10 +4,15 @@ from tourists.models import Tourist, Group
 
 # Create your views here.
 def crm(request):
-    groups = Group.objects.first
-    tourists = Tourist.objects.all()
+    groups = Group.objects.values('group_name')
+
+    tourists = Tourist.objects.first()
+    gantt = tourists.gantt_to_html()
+
     context = {
         'groups': groups,
         'tourists': tourists,
+        'gantt': gantt,
+
     }
     return render(request, 'crm.html', context=context)
