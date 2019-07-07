5c1302a4037bb76359ad90b1382841ed1105a258
supereko <elenakozina84@mail.ru>
2019-07-07 21:50:44 +1000

    удалено:       templates/registration/logged_out.html 	удалено:       templates/registration/login.html 	удалено:       templates/registration/password_reset_complete.html 	удалено:       templates/registration/password_reset_confirm.html 	удалено:       templates/registration/password_reset_done.html 	удалено:       templates/registration/password_reset_email.html 	удалено:       templates/registration/password_reset_form.html 	изменено:      tourism/settings.py 	изменено:      tourism/urls.py 	изменено:      tourists/admin.py 	изменено:      tourists/models.py 	изменено:      tourists/templates/tourists/show_list_services.html 	изменено:      tourists/views.py

--- tourism/urls.py
+++ tourism/urls.py
@@ -5,23 +5,13 @@ from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import url
-from django.views.generic import TemplateView
 
-from django.contrib.auth.decorators import user_passes_test
 
 from tourists import views
 
 urlpatterns = [
     path('crm/', include('overview.urls')),
     path('admin/', admin.site.urls),
-    #path('tourists/', include('tourists.urls')),
-    #path('', RedirectView.as_view(url='/tourists/')),
     path('', RedirectView.as_view(url='/admin/'), name='home'),
-    path('list_of_services/<int:pk>', views.show_list_services,
-        	name='show_list_services'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# Add Django site authentication urls (for login, logout, password management)
-urlpatterns += [
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-]
